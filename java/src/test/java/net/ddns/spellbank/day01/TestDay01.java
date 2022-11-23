111e0d596bbcde54960f1836a18693d1bacaa874
Josh Phillips <joshua.e.phillips@gmail.com>
2022-11-23 00:57:04 -0600

    Update settings for 2022, fix deprecated property

---
+++ java/src/test/java/net/ddns/spellbank/day01/TestDay01.java
@@ -0,0 +1,22 @@
+package net.ddns.spellbank.day01;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import net.ddns.spellbank.utils.InputFile;
+
+class TestDay01 {
+
+    @Test
+    void part1() {
+        String[] lines = InputFile.getLines("day01/input1");
+        assertEquals(0, Day01.part1(lines));
+    }
+
+    @Test
+    void part2() {
+        String[] lines = InputFile.getLines("day01/input1");
+        assertEquals(0, Day01.part2(lines));
+    }
+}
